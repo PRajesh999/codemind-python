ce221e946414016956f5f3fef45d9fdb5ac99b79
Narasimha Rajesh Pulugurtha <114941231+PRajesh999@users.noreply.github.com>
2023-05-10 18:58:30 +0530

    n=int(input()) sq=n*n rev=0 while(n>0):     r=n%10     rev=rev*10+r     n=n//10 x=rev*rev y=0 while(x>0):     p=x%10     y=y*10+p     x=x//10 if(y==sq):     print(True) else:     print(False)

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+n=int(input())
+sq=n*n
+rev=0
+while(n>0):
+    r=n%10
+    rev=rev*10+r
+    n=n//10
+x=rev*rev
+y=0
+while(x>0):
+    p=x%10
+    y=y*10+p
+    x=x//10
+if(y==sq):
+    print(True)
+else:
+    print(False)
